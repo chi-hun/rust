0991f3d455674bc923bc9eb91ade0502e9c45a1f
치훈 김 <huunnii@gmail.com>
2025-05-02 05:15:28 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+struct psize {
+    width: u32,
+    height: u32,
+}
+
+
+fn main() {
+    let size_wh = psize{
+        width : 40,
+        height : 150,
+    };
+    dbg!(&size_wh);
+    let area = ca_size(&size_wh);
+    println!("area : {}", area);
+
+}
+
+fn ca_size(wh: &psize) -> u32 {
+    wh.width * wh.height
+}
