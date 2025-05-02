d8726975c94c30b1b116719849cdee4b992bc670
치훈 김 <huunnii@gmail.com>
2025-05-02 06:42:06 +0000

    some change

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 #[derive(Debug)]
-struct psize {
+struct Psize {
     width: u32,
     height: u32,
 }
 
+impl Psize {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn scope(&self, sc: u32) -> u32 {
+        self.width * self.height * sc
+    }
+}
 
 fn main() {
-    let size_wh = psize{
+    let size_wh = Psize{
         width : 40,
         height : 150,
     };
     dbg!(&size_wh);
-    let area = ca_size(&size_wh);
-    println!("area : {}", area);
-    println!("haha");
-
-}
-
-fn ca_size(wh: &psize) -> u32 {
-    wh.width * wh.height
+    println!("area : {}", size_wh.area());
+    println!("scope : {}", size_wh.scope(2));
 }
