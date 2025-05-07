38ce848d0ea66b9cc0ec3019a4751a7564aab8d6
치훈 김 <huunnii@gmail.com>
2025-05-07 08:34:52 +0000

    some change

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,9 @@
-#[derive(Debug)]
-struct Psize {
-    width: u32,
-    height: u32,
-}
-
-impl Psize {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    fn scope(&self, sc: u32) -> u32 {
-        self.width * self.height * sc
-    }
-    fn make_squre(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,  
-        }
-    }
-}
-
+//8.2
+// #[derive(Debug)]
 fn main() {
-    let size_wh = Psize{
-        width : 40,
-        height : 150,
-    };
-    dbg!(&size_wh);
-    println!("area : {}", size_wh.area());
-    println!("scope : {}", size_wh.scope(2));
-    dbg!(Psize::make_squre(100));
+    let old_ss = "old";
+    let ss: String = String::new();
+    let mut ss = old_ss.to_string(); 
+    ss.push_str("_new");
+    println!("{}",ss);
 }
