3296d2f15e8c8488b0a1e2b142742d5f4632fcf3
치훈 김 <huunnii@gmail.com>
2025-05-08 08:37:45 +0000

    some change

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 //8.2
 // #[derive(Debug)]
+use std::fs::File;
+
 fn main() {
-    let s1 = String::from("안녕");
-    let s2 = String::from(" 하세요");
-    let s3 = s1 + &s2;
-    println!("{}", s3);
-    println!("{}", &s3[0..3]);
-    for i in s3.chars() {
-        println!("{}", i);
-    }
-    println!("{}", s3);
+    let file_load_result = File::open("input.txt").expect("fuck");
 }
