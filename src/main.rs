c0131cb0a46685bc78ad547de625fce89b248687
치훈 김 <huunnii@gmail.com>
2025-05-08 01:41:24 +0000

    some change

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 //8.2
 // #[derive(Debug)]
 fn main() {
-    let old_ss = "old";
-    let ss: String = String::new();
-    let mut ss = old_ss.to_string(); 
-    ss.push_str("_new");
-    println!("{}",ss);
+    let s1 = String::from("안녕");
+    let s2 = String::from(" 하세요");
+    let s3 = s1 + &s2;
+    println!("{}", s3);
+    println!("{}", &s3[0..3]);
+    for i in s3.chars() {
+        println!("{}", i);
+    }
+    println!("{}", s3);
 }
